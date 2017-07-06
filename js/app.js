65c45bd4c8abaa2d47b8d1b30b044a65cb263d16
Mickael <polmickael3@gmail.comgit config --global user.name>
2017-07-06 14:49:27 +0200

    commentaire

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$( '#addText' ).text('hello world'); 
+$('#addText' ).text('hello world'); /*ou $('#addText').html('hello world'); */
 console.log('hello world');
 
 $('#remove').remove();
@@ -7,6 +7,7 @@ $('#changeColor').addClass('backgroundcolor');
 //$('.backgroundcolor')
 
 
-for (var i = 0; i < 4; i++) {
-	$('#addChildren').append('<p>hello </p>');
+for (var i = 0; i < 4; i++) // défini la boucle jusqu'a 4
+{
+	$('#addChildren').append('<p>hello </p>'); // selectionne l'id et y ajoute les balise <p></p>
 }
